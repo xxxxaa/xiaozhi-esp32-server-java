cb73c9929828f9a9947b6182167b55d0f67f832d
JoeyZhou <zzq980308zzq@gmail.com>
2025-06-17 23:05:10 +0800

    fix:修复对话管理页面模型角色没有正确展示的问题

--- src/main/java/com/xiaozhi/entity/SysMessage.java
+++ src/main/java/com/xiaozhi/entity/SysMessage.java
@@ -55,6 +55,7 @@ public class SysMessage extends Base {
     //辅助字段，不对应数据库表
     private String sessionId;
     private Integer roleId;
+    private String roleName;
     private String deviceName;
 
     public String getDeviceId() {
@@ -133,6 +134,14 @@ public class SysMessage extends Base {
 
     public void setRoleId(Integer roleId) {
         this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
     }
 
     public String getDeviceName() {
